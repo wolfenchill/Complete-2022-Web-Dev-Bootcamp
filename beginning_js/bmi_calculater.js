30dc0bfd621152be4e7e973a663d5ba77563b8a6
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2022-05-24 12:11:13 -0700

    Create bmi_calculater.js

---
+++ beginning_js/bmi_calculater.js
@@ -0,0 +1,21 @@
+function calculateBMI(weight, height) {
+
+    var calc = weight / ((height) ** 2)
+    return calc
+
+
+}
+
+
+
+var bmi = calculateBMI(65, 1.8);
+
+// console.log(bmi);
+
+function aFunction() {
+    var a = 12;
+    var b = 7;
+    return a - b;
+}
+
+console.log(aFunction())
