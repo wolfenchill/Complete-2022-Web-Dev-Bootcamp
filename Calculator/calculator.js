6c1f6d054ae0c27a0a56ca82710f9eb973592ff4
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2022-08-09 11:45:55 -0700

    BMI Calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
-    res.send("thanks for posting that");
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+    res.send(`The result of the calculation is ${result}`);
+});
+
+app.get("/bmiCalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.listen(3000, function() {
