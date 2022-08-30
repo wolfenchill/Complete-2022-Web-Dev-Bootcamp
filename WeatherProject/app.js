842420189430f5e15c4c11a4df10ed5cd5030c45
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2022-08-30 08:14:33 -0700

    update

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
-
+const https = require("https");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Server is up and running.");
+    const url =
+        "https://api.openweathermap.org/data/2.5/weather?q=London&appid=96e3eb63ff83a6ebfbaf48fe909c248f";
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+        response.on("data", function(data) {
+            console.log(data);
+        });
+        res.send("Server is up and running.");
+    });
 });
 
 app.listen(3000, function() {
