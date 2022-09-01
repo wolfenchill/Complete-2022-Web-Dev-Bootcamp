6a5974d72b8a3beaecb2e88bb63f10ddcb659d17
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2022-09-01 09:49:20 -0700

    Update app.js

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -8,35 +8,42 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
-    console.log("Post Request Received");
-});
-
-// const url =
-//     "https://api.openweathermap.org/data/2.5/weather?q=London&appid=96e3eb63ff83a6ebfbaf48fe909c248f&units=imperial";
-// https.get(url, function(response) {
-//     console.log(response.statusCode);
-//     response.on("data", function(data) {
-//         const weatherData = JSON.parse(data);
-//         const temp = weatherData.main.temp;
-//         console.log(temp);
-//         const description = weatherData.weather[0].description;
-//         console.log(description);
-//         const icon = weatherData.weather[0].icon;
-//         const displayTemp =
-//             "<h1>The temperature in London is " + temp + " degrees Fahrenheit</h1>";
-//         const displayDescription =
-//             "<h1>The weather is currently " + description + "</h1>";
-
-//         const imgURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-//         res.write(displayTemp);
-//         res.write(displayDescription);
-//         res.write(`<img src = ${imgURL}>`);
-//         res.send();
-// });
-// });
-// });
+// res.write(displayTemp);
+// res.write(displayDescription);
+// res.write(`<img src = ${imgURL}>`);
+// res.send();
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
+});
+
+app.post("/", function(req, res) {
+    const userInput = req.body.cityName;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=96e3eb63ff83a6ebfbaf48fe909c248f&units=imperial`;
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            console.log(temp);
+            const description = weatherData.weather[0].description;
+            console.log(description);
+            const icon = weatherData.weather[0].icon;
+            const displayTemp =
+                "<h1>The temperature in " +
+                userInput +
+                " is " +
+                temp +
+                " degrees Fahrenheit</h1>";
+            const displayDescription =
+                "<h1>The weather is currently " + description + "</h1>";
+
+            const imgURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+            // res.write(displayTemp);
+            res.write(displayTemp);
+            res.write(displayDescription);
+            res.write(`<img src=${imgURL}>`);
+            res.send();
+        });
+    });
 });
