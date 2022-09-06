6e11f94d923b7a938edf871b36608c2c5bfb4e47
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2022-09-05 18:19:56 -0700

    Yes

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -7,3 +7,11 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+    console.log("received");
+});
